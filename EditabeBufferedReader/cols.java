f085246819701fc3a3541f7980d990a211f2f6a1
marias <jordimarias@gmail.com>
2018-03-02 11:01:09 +0100

    Comentaris F.Oller

--- EditabeBufferedReader/cols.java
+++ EditabeBufferedReader/cols.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
-public class cols{
+public class Cols {
 
 	public static void main(String[] args) throws IOException{
 		int cols, lines;
 	
 		try{
-
+			//Després de crítiques del Francesc Oller ens ha dir que hem de buscar una manera alternativa de realitzar això sense fer ús del /dev/tty el /bin/sh i el -c
 			Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "tput cols </dev/tty"});
 			cols = Integer.parseInt(new BufferedReader(new InputStreamReader(p.getInputStream())).readLine());
 			System.out.println("COL: " + cols);
