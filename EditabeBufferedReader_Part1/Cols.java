0fbaa9d48712dc5a601c25c3e2ec6ee61519a0a3
lluisbaro <lluis.baro.cayetano@gmail.com>
2018-03-23 22:35:46 +0100

    Alternatives de Cols

--- EditabeBufferedReader_Part1/Cols.java
+++ EditabeBufferedReader_Part1/Cols.java
@@ -1,12 +1,17 @@
-//import java.io.*;
+import java.io.*;
+//import EditableBufferedReader.java;
+
 
 class Cols {
 
 	public static void main(String[] args){
-		int cols, lines;
+		int cols=0, lines=0;
 	
-		//try{
-			//Després de crítiques del Francesc Oller ens ha dir que hem de buscar una manera alternativa de realitzar això sense fer ús del /dev/tty el /bin/sh i el -c
+		try{
+		//	EditableBufferedReader.setRaw();
+			
+			// Alternativa 1
+			
 			/*Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "tput cols </dev/tty"});
 			cols = Integer.parseInt(new BufferedReader(new InputStreamReader(p.getInputStream())).readLine());
 			System.out.println("COL: " + cols);
@@ -14,13 +19,59 @@ class Cols {
 			Process pl = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "tput lines </dev/tty"});
 			lines = Integer.parseInt(new BufferedReader(new InputStreamReader(pl.getInputStream())).readLine());
 			System.out.println("LINES: " + lines);*/
-                        /*Codi Definitiu per a trobar les columnes */
-                        cols = Integer.parseInt(System.getenv("COLUMNS"));
-                        lines = Integer.parseInt(System.getenv("LINES"));
-                        System.out.print("\nColumnes: "+cols+"\nLinies: "+lines+"\n\n");
-                        /*
-		}catch(IOException e){
-			e.printStackTrace();
-		}*/
+			
+			// Alternativa 2		/*Codi Definitiu per a trobar les columnes */
+			
+		    cols = Integer.parseInt(System.getenv("COLUMNS"));	// s'ha d'exportar la variable fer > export COLUMNS
+		    lines = Integer.parseInt(System.getenv("LINES"));
+		    
+        
+        	// Alternativa 3 		/*Codi per mitjançant les SEQ_ESC*/
+       		//CSI Ps ; Ps ; Ps 
+        	//CSI 18 ; height ; width t 
+		    // report size of text area in chars: CSI 18 t
+		    // should return ESC [ 8 ; rmax ; cmax t
+		    
+		    //System.out.print("\033[18t");
+		
+			// Alternativa 3
+			// ESC [ 8 ; rmax ; cmax t
+			/*
+			Scanner sc = new Scanner(System.in);
+			sc.skip("\033\\[8;\\d+;(\\d+)t");
+			cols = Integer.parseInt(sc.match().group(1));
+			*/
+			
+			// Alternativa 4
+			/*
+			Scanner sc = new Scanner(System.in);
+			sc.skip("\033\\[8;").useDelimiter(";");
+			sc.nextInt(); // skip rmax
+			sc.skip(";").useDelimiter("t");
+			cols = sc.nextInt();
+			sc.skip("t");
+			*/
+			
+			// Alternativa 5
+			/*
+			String str = readAll(new BufferedReader(new InputStreamReader(System.in)));
+			String cmax = str.substring(
+				str.indexOf(";", str.indexOf(";") + 1) + 1,
+				str.length() - 1);
+			cols = Integer.parseInt(cmax);
+			*/
+			
+			// Alternativa 6	//alguna amb metodes de pel mig que no he copiat
+			// ESC [ 8 ; rmax ; cmax t
+			/*
+			BufferedReader bin = new BufferedReader(new InputStreamReader(System.in));
+			
+		    */
+        
+		} finally {
+	//	EditableBufferedReader.unsetRaw();
+		}
+		// Printem les variables
+		System.out.print("\nColumnes: "+cols+"\nLinies: "+lines+"\n\n");
 	}
 }
