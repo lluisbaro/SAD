6be8f84e666a5e53836e06c38db814398df403e8
lluisbaro <lluis.baro.cayetano@gmail.com>
2018-03-23 10:53:32 +0100

    Cols Actualitzat als dos direcotis

--- View_Part2/Cols.java
+++ View_Part2/Cols.java
@@ -1,22 +1,26 @@
-import java.io.*;
+//import java.io.*;
 
-public class Cols {
+class Cols {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		int cols, lines;
 	
-		try{
+		//try{
 			//Després de crítiques del Francesc Oller ens ha dir que hem de buscar una manera alternativa de realitzar això sense fer ús del /dev/tty el /bin/sh i el -c
-			Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "tput cols </dev/tty"});
+			/*Process p = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "tput cols </dev/tty"});
 			cols = Integer.parseInt(new BufferedReader(new InputStreamReader(p.getInputStream())).readLine());
 			System.out.println("COL: " + cols);
 
 			Process pl = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "tput lines </dev/tty"});
 			lines = Integer.parseInt(new BufferedReader(new InputStreamReader(pl.getInputStream())).readLine());
-			System.out.println("LINES: " + lines);
-
+			System.out.println("LINES: " + lines);*/
+                        /*Codi Definitiu per a trobar les columnes */
+                        cols = Integer.parseInt(System.getenv("COLUMNS"));
+                        lines = Integer.parseInt(System.getenv("LINES"));
+                        System.out.print("\nColumnes: "+cols+"\nLinies: "+lines+"\n\n");
+                        /*
 		}catch(IOException e){
 			e.printStackTrace();
-		}
+		}*/
 	}
 }
