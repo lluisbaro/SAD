02117b2afac4da0b6a287beee47d0a5bd8ca85d5
lluisbaro <lluis.baro.cayetano@gmail.com>
2018-05-04 13:50:08 +0200

    clientcontroller javascript pou

--- WebClient/ClientController.js
+++ WebClient/ClientController.js
@@ -4,7 +4,8 @@ function ClientController(address, nick){
 
   this.webSocket = new WebSocket(address);
   this.nick = nick;
-  this.messages = new Messages();
+  //this.messages = new Messages();		esta dins de a funcio onmessage
+
 
   function send(message){
     this.webSocket.send(this.nick+'&%:'+message);
@@ -12,11 +13,12 @@ function ClientController(address, nick){
 
   this.webSocket.onmessage = function(event){
     //Aqui s'implementen funcions de control;
-
+  	this.messages = new Messages();	
     //Aqui guardem el missatge rebut al model;
-    var dades = this.event.data.split('&%:');
+    var dades = event.data.split('&%:');
     var missatge = new Message(dades[0], dades[1]);
-    this.messages.put(missatge);
+    this.messages.push(missatge);
+    refresh();
   }
   //this.webSocket.onclose = function(event){
 
@@ -25,3 +27,25 @@ function ClientController(address, nick){
 }
 
 //Al connectar s'envia el nick amb el següent format: &% Nick: <Nick>&%
+
+//Client MOdel
+
+function Message(nick, message){
+  this.message = message;
+  this.nick = nick;
+
+  function toString(){
+    return this.nick+': '+this.message;
+  }
+}
+
+function Messages(){
+
+  this.messages = new Array();
+
+  function put(message){
+    this.messages.push(message);
+    //Despres de l'actualitzacio hem d'avisar a la vista
+    refresh();
+  }
+}
