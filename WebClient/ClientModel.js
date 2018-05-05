111611e3cfaf5ab1c28febf199273b9ebe583427
marias <jordimarias@gmail.com>
2018-05-05 18:36:54 +0200

    Funciona!

---
+++ WebClient/ClientModel.js
@@ -0,0 +1,17 @@
+function Message(nick, message){
+  this.message = message;
+  this.nick = nick;
+
+  function toString(){
+    return this.nick+': '+this.message;
+  }
+}
+
+function Messages(){
+  this.messages = new Array();
+  this.messages.push(new Message('Missatge', 'Inicial'));
+}
+Messages.prototype.put = function(message){
+  this.messages.push(message);
+  refresh(message);
+}
