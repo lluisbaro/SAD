07b688c052cb09e7eb3e666b7c7a31c785c8b2c1
marias <jordimarias@gmail.com>
2018-05-08 19:03:06 +0200

    Nick handshake ended

--- WebServer/src/ChatServer/ChatServer.java
+++ WebServer/src/ChatServer/ChatServer.java
@@ -61,7 +61,7 @@ public class ChatServer extends WebSocketServer {
 
 	@Override
 	public void onOpen( WebSocket conn, ClientHandshake handshake ) {
-		conn.send("Benvingut a la sala!"); //This method sends a message to the new client
+		//conn.send("Benvingut a la sala!"); //This method sends a message to the new client
 		//broadcast( "new connection: " + handshake.getResourceDescriptor() + conn.toString()); //This method sends a message to all clients connected
 		//System.out.println( conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!" );
 	}
